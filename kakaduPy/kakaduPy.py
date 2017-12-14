89209659a0034528bcac226ed71f6f417ce718f1
bitsgalore <johan.vanderknijff@kb.nl>
2017-12-14 13:20:19 +0100

    added metadata extraction option

--- kakaduPy/kakaduPy.py
+++ kakaduPy/kakaduPy.py
@@ -3,6 +3,8 @@
 
 import sys
 import os
+import uuid
+import io
 import subprocess as sub
 import csv
 
@@ -15,7 +17,7 @@ kdu_compress = "/home/johan/kakadu/kdu_compress"
 # To add it, use:
 # export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:"/home/johan/kakadu/
 
-# Path to ExifTool binary
+# Path to ExifTool binary (only needed if extractMetadataFlag is set to True)
 global exiftool
 exiftool = "/usr/bin/exiftool"
 
@@ -44,20 +46,15 @@ def launchSubProcess(args):
 
     return(exitStatus, outputAsString, errorsAsString)
 
-
-def compressToJP2(fileIn, fileOut, parameters):
-    """Compress image to JP2"""
+def extractMetadata(fileIn):
+    """Extract metadata to XMP format (written to stdout)"""
 
     # Add command-line arguments
-    args = [kdu_compress]
-    args.append("-i")
+    args = [exiftool]
     args.append(fileIn)
     args.append("-o")
-    args.append(fileOut)
+    args.append("-.xmp")
 
-    for p in parameters:
-        args.append(p)
-    
     # Command line as string (used for logging purposes only)
     cmdStr = " ".join(args)
 
@@ -72,6 +69,61 @@ def compressToJP2(fileIn, fileOut, parameters):
 
     return dictOut
 
+
+def compressToJP2(fileIn, fileOut, parameters, extractMetadataFlag):
+    """Compress image to JP2
+    if extractMetadataFlag is True, Exiftool is used to extract metadata from
+    fileIn in, which is subsequently written to an XMP sidecar file and then
+    embedded in an XML box (as per KB specs). However, by default Kakadu
+    already does the metadata extraction natively, but it uses the uuid box!
+    """
+
+    # Add command-line arguments
+    args = [kdu_compress]
+    args.append("-i")
+    args.append(fileIn)
+    args.append("-o")
+    args.append(fileOut)
+
+    for p in parameters:
+        args.append(p)
+
+    if extractMetadataFlag:
+        # Extract metadata from fileIn to XMP
+        resultExiftool = extractMetadata(fileIn)
+
+        # XMP data in stdout
+        xmpData = resultExiftool["stdout"]
+
+        # Prepend xmpData with string "xml "
+        xmpData = "xml "+ xmpData
+
+        # Write xmpData to temporary file
+        xmpFNameTemp = str(uuid.uuid1())
+        with io.open(xmpFNameTemp, "w", encoding="utf-8") as fXMP:
+            fXMP.write(xmpData)
+        fXMP.close()
+
+        args.append("-jp2_box")
+        args.append(xmpFNameTemp)
+    
+    # Command line as string (used for logging purposes only)
+    cmdStr = " ".join(args)
+
+    status, out, err = launchSubProcess(args)
+
+    # Main results to dictionary
+    dictOut = {}
+    dictOut["cmdStr"] = cmdStr
+    dictOut["status"] = status
+    dictOut["stdout"] = out
+    dictOut["stderr"] = err
+
+    if extractMetadataFlag:
+        os.remove(xmpFNameTemp)
+
+    return dictOut
+
 def convertDirectory(dirIn, ExtIn, dirOut):
     """Convert all files with extension ExtIn in directory dirIn to JP2 in directory dirOut"""
     pass
@@ -82,6 +134,7 @@ def main():
     # Input
     imageIn = "/home/johan/handschriften/tiff/KBHSS01000058055/424C1-02-02_0208.tif"
     imageOut = "/home/johan/test/test.jp2"
+    extractMetadataFlag = False
 
     # Parameters for lossy compression of  RGB image at 20:1 ratio
     # TODO: add XMP metadata, codestream comment
@@ -96,10 +149,11 @@ def main():
               "1.2,0.6,0.3,0.15,0.075,0.0375,0.01875,0.009375",
               "Cuse_sop=yes",
               "Cuse_eph=yes",
-              "Cmodes=SEGMARK"]
+              "Cmodes=SEGMARK",
+              "-com",
+              "KB_ACCESS_LOSSY_01/01/2015"]
 
-    resultKakadu = compressToJP2(imageIn, imageOut, params)
-    print(resultKakadu)
+    resultKakadu = compressToJP2(imageIn, imageOut, params, extractMetadataFlag)
 
 
 if __name__ == "__main__":
